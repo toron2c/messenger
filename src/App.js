9f42c493251981a54f18668b2582ee838c3b42f4
hanbaske <hanbaske@student.21-schol.ru>
2022-11-05 18:34:11 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import './App.css';
+import { useEffect, useState } from 'react';
+import './App.scss';
 import { Message } from './components/Message/Message';
 
 function App() {
-  const Text = "Hello? How Are you?"
-  return ( <div>
-    <Message Message={Text} />
+  const [messageList, setMessageList] = useState( [] );
+  const [textMessage, setTextMessage] = useState( "" );
+  const [isTime, setIsTime] = useState( false );
+  useEffect( () => {
+    if ( messageList.length > 0 && messageList[messageList.length - 1].author === 'user' ) {
+      let timer = setTimeout( () => {
+        setMessageList( [...messageList, { text: "Я всего лишь робот, который умеет только писать это сообщение :(" }] );
+        setIsTime( false );
+      }, 1500 )
+      return () => {
+        clearTimeout( timer );
+      }
+    }
+
+  }, [messageList] )
+
+  const onChangeInput = ( e ) => {
+    e.preventDefault();
+    setTextMessage( e.target.value );
+  }
+  const onSendMessage = ( e ) => {
+    e.preventDefault();
+    if ( textMessage === '' ) return;
+    setMessageList( [...messageList, { text: textMessage, author: 'user' }] );
+    setTextMessage( '' );
+    setIsTime( true );
+  }
+  let messages = messageList.map( ( el, idx ) => <Message Message={el.text} author={el.author} key={idx} /> );
+  return ( <div className='box'>
+    <div className='messages'>
+      {messages && messages}
+    </div>
+    <div className='controllers'>
+      {isTime ? <div className='textAlert'>Подождите! Робот, думает что ответить.....</div> : <div className='controllersInput'><textarea type={'text'} value={textMessage} onChange={onChangeInput} />
+        <button onClick={onSendMessage}>send message</button></div>}
+    </div>
   </div>
   )
 }
--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,8 +1,8 @@
 import style from './message.module.scss'
 
 
-export function Message( { Message } ) {
-    return ( <div className={style.box}>
+export function Message( { Message, author } ) {
+    return ( <div className={author}>
         <div className={style.text}>{Message}</div>
     </div> )
 }
