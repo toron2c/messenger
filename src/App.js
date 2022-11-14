31a8146e68fd2c89b2cad323fe7f59f84bdef99e
hanbaske <hanbaske@student.21-school.ru>
2022-11-14 18:31:56 +0300

    tmp_3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Chats from './components/Chats/Chats';
+import Home from './components/Home/Home';
 import Menu from './components/Menu/Menu';
+import Profile from './components/Profile/Profile';
 
 
 function App() {
@@ -8,8 +11,13 @@ function App() {
 
   return ( <div>
     <div className='box'>
-      <Menu />
-      <Chats />
+      <Routes>
+        <Route path={'/'} element={<Menu />}>
+          <Route index element={<Home />} />
+          <Route path={'profile'} element={<Profile />} />
+          <Route path={'chats'} element={<Chats />} />
+        </Route>
+      </Routes>
     </div>
   </div>
   )
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,21 +1,35 @@
-import { Breadcrumbs, Link } from '@mui/material'
+import { Button } from '@mui/material'
+import { NavLink, Outlet } from 'react-router-dom'
 import style from './Menu.module.scss'
 
 export default function Menu() {
 
-    return ( <div className={style.menu}>
-        <nav>
-            <Breadcrumbs aria-label="breadcrumb" separator="">
-                <Link underline="hover" className={style.link}>
-                    Home
-                </Link>
-                <Link underline="hover" className={style.link}>
-                    Profile
-                </Link>
-                <Link underline="hover" className={style.link}>
-                    Chats
-                </Link>
-            </Breadcrumbs>
-        </nav>
-    </div> )
+    return ( <div className={style.app}>
+        <div className={style.menu}>
+            <nav>
+                <ul className={style.list}>
+                    <li><NavLink className={style.link} to="/">
+                        {( { isActive } ) => (
+                            isActive ? <Button variant="contained" style={{ padding: '7px 35px' }}>Home</Button> :
+                                <Button style={{ padding: '7px 35px' }} variant="outlined">Home</Button>
+                        )}
+                    </NavLink></li>
+                    <li><NavLink className={style.link} to="/profile">
+                        {( { isActive } ) => (
+                            isActive ? <Button style={{ padding: '7px 35px' }} variant="contained" href="#contained-buttons" >Profile</Button> :
+                                <Button style={{ padding: '7px 35px' }} variant="outlined">Profile</Button>
+                        )}
+                    </NavLink></li>
+                    <li><NavLink className={style.link} to="/chats">
+                        {( { isActive } ) => (
+                            isActive ? <Button style={{ padding: '7px 35px' }} variant="contained" href="#contained-buttons" >Chats</Button> :
+                                <Button style={{ padding: '7px 35px' }} variant="outlined">Chats</Button>
+                        )}
+                    </NavLink></li>
+                </ul>
+            </nav>
+        </div>
+        <Outlet />
+    </div>
+    )
 }
