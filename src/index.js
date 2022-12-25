fc1a2edf6754d0f47ca1349c8357f7a520f2a2bb
toron2c <toron2c@yandex.ru>
2022-12-25 16:02:33 +0300

    beta0.11

--- src/index.js
+++ src/index.js
@@ -16,15 +16,15 @@ sagaMiddleWare.run( rootWatcher );
 
 const root = ReactDOM.createRoot( document.getElementById( 'root' ) );
 root.render(
-    // <React.StrictMode>
-    <Provider store={store}>
-        <PersistGate persistor={persistor}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </PersistGate>
-    </Provider>
-    // </React.StrictMode >
+    <React.StrictMode>
+        <Provider store={store}>
+            <PersistGate persistor={persistor}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </PersistGate>
+        </Provider>
+    </React.StrictMode >
 );
 
 // If you want to start measuring performance in your app, pass a function
