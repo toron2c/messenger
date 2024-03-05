0864e134e756d9bb40d5a47ce54576fd9f64fdf9
follik42 <toron2c@yandex.ru>
2024-03-05 19:43:50 +0300

    start new design 2k24_03

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,9 @@ export default function Home( { isAuth } ) {
     return (
         <div className={style.home}>
             <Typography className={style.logo} variant="h1">Messenger 2000</Typography>
-            {!isAuth && <HomeLogin />}
+            {!isAuth && <div><Typography className={style.home__hi} variant="h4">Добро пожаловать!</Typography></div>}
+						{!isAuth && <div><Typography className={style.home__hi} variant="subtitle1">Вы можете авторизоваться или зарегистрирватьсяё</Typography></div>}
+						{!isAuth && <HomeLogin />}
         </div>
     )
 }
