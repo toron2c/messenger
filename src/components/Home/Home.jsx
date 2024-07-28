65c92ca237906c07adbc1820e0a3113fdf6006f8
toron2c <follik42@gmail.com>
2024-07-28 12:03:32 +0300

    add ad

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,24 +1,36 @@
-import style from './Home.module.scss'
+import style from "./Home.module.scss";
 import { HomeLogin } from "./HomeLogin/HomeLogin";
 
-export default function Home( { isAuth } ) {
-    return (
-        <div className={style.home}>
-            <h1 className={style.home__logo} variant="h1">{'<'}Messenger 2000{'/>'}</h1>
-            {!isAuth && <>
-							<p className={style.home__text}>Добро пожаловать!</p>
-							<p className={style.home__text}>
-								Вы можете зарегистрирваться или авторизоваться.
-							</p>
-						</>}
-						{!isAuth && <HomeLogin />}
-						{
-						isAuth && 
-						<>
-							<p className={style.home__text}>Вы находитесь на сайте Messenger2000</p>
-							<p className={style.home__text}>Мессенджер в реальном времени  </p>
-						</>
-						}
-        </div>
-    )
+export default function Home({ isAuth }) {
+  return (
+    <div className={style.home}>
+      <h1 className={style.home__logo} variant="h1">
+        {"<"}Messenger 2000{"/>"}
+      </h1>
+      <div class={style.ad}>
+        В поисках дизайнера
+        <p>telegram: @toron2c</p>
+        <a class={style.link} href="https://t.me/toron2c">
+          ссылка
+        </a>
+      </div>
+      {!isAuth && (
+        <>
+          <p className={style.home__text}>Добро пожаловать!</p>
+          <p className={style.home__text}>
+            Вы можете зарегистрирваться или авторизоваться.
+          </p>
+        </>
+      )}
+      {!isAuth && <HomeLogin />}
+      {isAuth && (
+        <>
+          <p className={style.home__text}>
+            Вы находитесь на сайте Messenger2000
+          </p>
+          <p className={style.home__text}>Мессенджер в реальном времени </p>
+        </>
+      )}
+    </div>
+  );
 }
